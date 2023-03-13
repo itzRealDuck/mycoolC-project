aab336bf4fffe15084cd572c79c3889ca69cba50
itzRealduck <107372053+itzRealDuck@users.noreply.github.com>
2023-03-13 22:17:44 +0200

    did the random event gen

--- randomeventgenerator.cpp
+++ randomeventgenerator.cpp
@@ -5,21 +5,27 @@ int main(){
 
 srand(time(0)); 
 
-int eventgen = (rand() % 10) + 1
+int eventgen = (rand() % 6) + 1;
 
 switch(eventgen){  
 
-    case 1:
-case 1: 
-case 1: 
-case 1: 
-case 1: 
-case 1: 
-case 1: 
-case 1: 
-case 1: 
-case 1:
+    case 1:  
+    std::cout << "it is raining today";
+    break;
+case 2: std::cout << "it is hot today";
+break; 
 
+case 3:  
+std::cout << "there is ACTUAL snow outside";
+break;  
+case 4: std::cout << "damn its pretty cold";
+break; 
+
+case 5: std::cout << "it is ramadan";
+break; 
+
+case 6: std::cout << "it is eid";
+break;
 
 }
 
